03318b9f19b15333110ca055a661aeffdbd261f0
Ben Levy <evilneb3.14@gmail.com>
2021-08-15 16:09:17 -0400

    more restructuring and implement 'useEffect' hook to run repetitive propogation

--- src/App.js
+++ src/App.js
@@ -4,71 +4,80 @@ import Row from './grid-components/row';
 
 import produce from 'immer';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+function initializeGrid(model) {
+  const cellGrid = [];
+  for(let y = 0; y < model.height; y++) {
+    const cells = [];
+    for(let x = 0; x < model.width; x++) {
+      const cell = {
+        alive: model.cellStates[y + x],
+        coordinates: { X: x, Y: y }
+      }
+      cells.push(cell);
+    }
+    cellGrid.push(cells);
+  }
+  return cellGrid;
+}
+
+const rowCount = 30;
+const columnCount = 50;
+
+const model = {
+  width: columnCount,
+  height: rowCount,
+  cellStates: Array(columnCount * rowCount).fill(0)
+}
 
 function App() {
-
-  const rowCount = 5;
-  const columnCount = 5;
-
-  const model = {
-    width: columnCount,
-    height: rowCount,
-    cellStates: Array(columnCount * rowCount).fill(0)
-  }
 
   const [grid, setGrid] = useState(() => initializeGrid(model));
 
   const [timeTicking, setTimeTicking] = useState(false);
 
-  function initializeGrid(model) {
-    const cellGrid = [];
-    for(let y = 0; y < model.height; y++) {
-      const cells = [];
-      for(let x = 0; x < model.width; x++) {
-        const cell = {
-          alive: model.cellStates[y + x],
-          coordinates: { X: x, Y: y }
-        }
-        cells.push(cell);
-      }
-      cellGrid.push(cells);
-    }
-    return cellGrid;
-  }
+  useEffect(() => {
+    if(timeTicking)
+      propogate(500);
+  });
   
-  function determinePropogation(cell) {
-    const x = cell.coordinates.X;
-    const y = cell.coordinates.Y;
-    const neighbors = [
-      grid[y-1]?.[x-1],
-      grid[y]?.[x-1],
-      grid[y+1]?.[x-1],
-      grid[y-1]?.[x],
-      grid[y+1]?.[x],
-      grid[y-1]?.[x+1],
-      grid[y]?.[x+1],
-      grid[y+1]?.[x+1]
-    ];
-    const neighborCount = neighbors.filter(neighbor => neighbor !== undefined && neighbor.alive).length;
-    const willPropogate = (cell.alive && neighborCount === 2) || neighborCount === 3; 
-    return willPropogate;
-  }
-  
-  function tickTock() {
-    const nextGrid = produce(grid, gridDraft => {
-      for(let y = 0; y < grid.length; y++) {
-        for(let x = 0; x < grid[y].length; x++) {
-          const cell = grid[y][x];
-          gridDraft[y][x] = {
-            alive: determinePropogation(cell),
-            coordinates: { X: x, Y: y }
-          };
+  function propogate(ms) {
+    setTimeout(() => {
+      const nextGrid = produce(grid, gridDraft => {
+        for(let y = 0; y < grid.length; y++) {
+          for(let x = 0; x < grid[y].length; x++) {
+            const cell = grid[y][x];
+            gridDraft[y][x] = {
+              alive: determinePropogation(cell),
+              coordinates: { X: x, Y: y }
+            };
+          }
         }
-      }
-    });
+      });
 
-    setGrid(nextGrid);
+      setGrid(nextGrid);
+
+      return !timeTicking;
+    }, ms);
+
+    function determinePropogation(cell) {
+      const x = cell.coordinates.X;
+      const y = cell.coordinates.Y;
+      const neighbors = [
+        grid[y-1]?.[x-1],
+        grid[y]?.[x-1],
+        grid[y+1]?.[x-1],
+        grid[y-1]?.[x],
+        grid[y+1]?.[x],
+        grid[y-1]?.[x+1],
+        grid[y]?.[x+1],
+        grid[y+1]?.[x+1]
+      ];
+      const neighborCount = neighbors.filter(neighbor => neighbor !== undefined && neighbor.alive).length;
+      const willPropogate = (cell.alive && neighborCount === 2) || neighborCount === 3; 
+      return willPropogate;
+    }
   }
   
   function handleClick(cell) {
@@ -81,25 +90,11 @@ function App() {
       };
     }));
   }
-
-  function tickTime() {
-    setTimeTicking(!timeTicking);
-    if(!timeTicking)
-      return;
-    run();
-  }
-
-  function run() {
-    if(!timeTicking)
-      return;
-    tickTock();
-    setTimeout(run, 500);
-  }
   
   return (
     <>
-    <button className="bg-green-500 hover:bg-green-700 text-white rounded font-bold" onClick={tickTime}>{timeTicking ? "Stop" : "Start"}</button>
-    {/* <button className="bg-green-500 hover:bg-green-700 text-white rounded font-bold" onClick={() => tickTime()}>Go</button> */}
+    <button className="bg-green-500 hover:bg-green-700 text-white rounded font-bold" onClick={() => setTimeTicking(!timeTicking)}>{timeTicking ? "Halt" : "Flow"}</button>
+    <button className="bg-green-500 hover:bg-green-700 text-white rounded font-bold" onClick={() => propogate(0)}>Step</button>
     <div className="min-h-screen flex flex-col justify-center">
       <div className="mx-auto">{
         <Grid grid={grid.map((row, y) => 
